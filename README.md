0b46a24373ba12bc808e93afb4008d9163364342
Eric Dorsey <eric@ericdorsey.com>
2021-08-24 11:55:51 +0000

    add example client

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Will use this to hook into Keycloak to allow users to login to their account wit
 ## How it makes money
 
 Allow users to pay ETH to [create an OpenID Connect client](https://www.ory.sh/hydra/docs/guides/oauth2-clients/).
+
+## Generate a client
+
+```
+docker-compose exec hydra \
+    hydra clients create \
+    --endpoint http://localhost:4445/ \
+    --id example-client4 \
+    --secret example-secret \
+    --grant-types client_credentials,authorization_code \
+    --callbacks http://localhost:4001/callback
+```
---
+++ example-client/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { auth } = require("express-openid-connect");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(
+  auth({
+    issuerBaseURL: "https://hydra.decacube.com",
+    baseURL: "http://localhost:4001",
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    clientAuthMethod: "client_secret_basic",
+    secret: process.env.OIDC_SECRET,
+    idpLogout: true,
+    authorizationParams: {
+      response_type: "code",
+      scope: "openid",
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  console.log(req.oidc.user);
+  res.send(`Hello, ${req.oidc.user.sub}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
