31d4da3869fb6b7945fa44312871bd5ea0957a71
Eric Dorsey <eric@ericdorsey.com>
2021-10-06 08:03:00 +0000

    refine signup flow

--- api/public/js/pages/signup.js
+++ api/public/js/pages/signup.js
@@ -1,27 +1,66 @@
 import * as ethers from '/js/modules/ethers/ethers.esm.min.js'
 
-if (typeof window.ethereum !== 'undefined') {
-  const provider = new ethers.providers.Web3Provider(window.ethereum)
+const enableEthereumButton = document.querySelector('#enableEthereum')
+const tezosButton = document.querySelector('#enableTezos')
+const solanaButton = document.querySelector('#enableSolana')
 
-  const ethereumButton = document.querySelector('#enableEthereum')
+const ethereumErrorDiv = document.querySelector('#ethereumError')
 
-  const challengeInput = document.querySelector('#challenge')
-  const addressInput = document.querySelector('#address')
-  const ensInput = document.querySelector('#ens')
-  const emailInput = document.querySelector('#email')
-  const twitterInput = document.querySelector('#twitter')
-  const urlInput = document.querySelector('#url')
-  const signatureInput = document.querySelector('#signature')
+const challengeInput = document.querySelector('#challenge')
+const addressInput = document.querySelector('#address')
+const ensInput = document.querySelector('#ens')
+const emailInput = document.querySelector('#email')
+const twitterInput = document.querySelector('#twitter')
+const urlInput = document.querySelector('#url')
+const signatureInput = document.querySelector('#signature')
+const waitingForSignatureDiv = document.querySelector('#waitingForSignature')
 
-  ethereumButton.addEventListener('click', async (e) => {
-    e.preventDefault()
+const selectChainSection = document.querySelector('#selectChain')
+const connectChainSection = document.querySelector('#connectChain')
+
+const profileTitleHeader = document.querySelector('#profileTitle')
+const profileSubtitleHeader = document.querySelector('#profileSubtitle')
+const profileUrlDiv = document.querySelector('#profileUrl')
+const profileTwitterDiv = document.querySelector('#profileTwitter')
+const profileEmailDiv = document.querySelector('#profileEmail')
+
+const signupForm = document.querySelector('#signupForm')
+
+const connectChain = () => {
+  selectChainSection.classList.add('is-hidden')
+  connectChainSection.classList.remove('is-hidden')
+}
+
+const selectChain = () => {
+  selectChainSection.classList.remove('is-hidden')
+  connectChainSection.classList.add('is-hidden')
+}
+
+enableEthereumButton.addEventListener('click', async (e) => {
+  e.preventDefault()
+
+  if (typeof window.ethereum !== 'undefined') {
+    ethereumErrorDiv.classList.add('is-hidden')
+    waitingForSignatureDiv.classList.add('is-hidden')
+
     const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
-    console.log({ accounts })
-    const ens = await provider.lookupAddress(accounts[0])
-    console.log({ account: accounts[0], ens })
 
-    addressInput.value = accounts[0]
-    ensInput.value = ens
+    connectChain()
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    const address = accounts[0]
+    const ens = await provider.lookupAddress(address)
+
+    addressInput.value = address
+
+    if (ens) {
+      profileTitleHeader.textContent = ens
+      profileSubtitleHeader.textContent = address
+    } else {
+      profileTitleHeader.textContent = address
+      profileSubtitleHeader.textContent = ''
+    }
 
     if (ens) {
       const resolver = await provider.getResolver(ens)
@@ -29,16 +68,63 @@ if (typeof window.ethereum !== 'undefined') {
       const url = await resolver.getText('url')
       const twitter = await resolver.getText('com.twitter')
 
-      console.log({ email, url, twitter })
+      if (url) {
+        const a = document.createElement('a')
+        const linkText = document.createTextNode(url)
+        a.appendChild(linkText)
+        a.title = url
+        a.target = '_blank'
+        a.href = `http://${url}`
+        profileUrlDiv.replaceChildren(a)
+        profileUrlDiv.classList.remove('is-hidden')
+      } else {
+        profileUrlDiv.classList.add('is-hidden')
+      }
 
-      emailInput.value = email
-      urlInput.value = url
-      twitterInput.value = twitter
+      if (twitter) {
+        const a = document.createElement('a')
+        const linkText = document.createTextNode(`@${twitter}`)
+        a.appendChild(linkText)
+        a.title = twitter
+        a.target = '_blank'
+        a.href = `http://twitter.com/${twitter}`
+        profileTwitterDiv.replaceChildren(a)
+        profileTwitterDiv.classList.remove('is-hidden')
+      } else {
+        profileTwitterDiv.classList.add('is-hidden')
+      }
+
+      if (email) {
+        const a = document.createElement('a')
+        const linkText = document.createTextNode(email)
+        a.appendChild(linkText)
+        a.title = email
+        a.target = '_blank'
+        a.href = `mailto:${email}`
+        profileEmailDiv.replaceChildren(a)
+        profileEmailDiv.classList.remove('is-hidden')
+      } else {
+        profileEmailDiv.classList.add('is-hidden')
+      }
     }
 
-    const signer = provider.getSigner()
-    const signature = await signer.signMessage(challengeInput.value)
+    waitingForSignatureDiv.classList.remove('is-hidden')
 
-    signatureInput.value = signature
-  })
-}
+    const signer = provider.getSigner()
+    try {
+      const signature = await signer.signMessage(challengeInput.value)
+
+      signatureInput.value = signature
+    } catch (err) {
+      selectChain()
+      return
+    }
+
+    waitingForSignatureDiv.classList.add('is-hidden')
+    console.log('SUBMITTING')
+    signupForm.submit()
+    console.log('HERE', signupForm)
+  } else {
+    ethereumErrorDiv.classList.remove('is-hidden')
+  }
+})
--- api/src/routes/signup.ts
+++ api/src/routes/signup.ts
@@ -1,5 +1,9 @@
 import express from 'express'
 import csrf from 'csurf'
+import urljoin from 'url-join'
+
+import asyncRoute from '../helpers/async-route'
+import generateChallenge from '../helpers/generate-challenge'
 
 // Sets up csrf protection
 const csrfProtection = csrf({
@@ -8,14 +12,23 @@ const csrfProtection = csrf({
 
 const router = express.Router()
 
-router.get('/', csrfProtection, (req: any, res: any) => {
-  res.render('signup', {
-    csrfToken: req.csrfToken()
+router.get(
+  '/',
+  csrfProtection,
+  asyncRoute(async (req: any, res: any) => {
+    const challenge = await generateChallenge()
+
+    res.render('signup', {
+      csrfToken: req.csrfToken(),
+      action: urljoin(process.env.BASE_URL || '', '/signup'),
+      challenge
+    })
   })
-})
+)
 
 router.post('/', csrfProtection, async (req: any, res: any) => {
   // The challenge is now a hidden input field, so let's take it from the request body instead
+  res.json(req.body)
 })
 
 export default router
